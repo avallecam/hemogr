9c4132898e11c757be15d51401db771dc7d38a3a
avallecam <avallecam@gmail.com>
2021-08-24 10:46:02 -0500

    add gametocyte plots

--- hemo_explore.R
+++ hemo_explore.R
@@ -12,14 +12,24 @@ hem <- readxl::read_excel("data-raw/hemdb-visitas-20210812.xlsx") %>%
   mutate(fiebre_2_ultimos_dias=if_else(fiebre_2_ultimos_dias=="sí",
                                        "si",
                                        fiebre_2_ultimos_dias)) %>% 
+  # parasitemia by concentration
   mutate(cruces_n=case_when(
+    is.na(diagnostico) ~ NA_integer_,
     str_detect(diagnostico,pattern = "\\+\\+\\+") ~ 3L,
     str_detect(diagnostico,pattern = "\\+\\+") ~ 2L,
     str_detect(diagnostico,pattern = "\\+") ~ 1L,
     str_detect(diagnostico,pattern = "Neg") ~ NA_integer_,
     TRUE ~ 0L
   )) %>% 
-  mutate(cruces_n=as.factor(cruces_n))
+  mutate(cruces_n=as.factor(cruces_n)) %>% 
+  # gametocytes
+  mutate(gametocytes_presence=case_when(
+    is.na(diagnostico) ~ NA_character_,
+    str_detect(diagnostico,pattern = "Neg") ~ NA_character_,
+    str_detect(diagnostico,pattern = "\\g") ~ "with",
+    TRUE ~ "without"
+  )) %>% 
+  mutate(gametocytes_presence=as.factor(gametocytes_presence))
 
 # visualizacion rapida de variables
 hem %>% glimpse()
@@ -37,16 +47,17 @@ hem %>%
 hem %>% 
   filter(!is.na(temperatura_celsius)) %>% 
   count(diagnostico,sort = T) %>% 
-  # mutate(cruces_n=case_when(
-  #   str_detect(diagnostico,pattern = "\\+\\+\\+") ~ 3L,
-  #   str_detect(diagnostico,pattern = "\\+\\+") ~ 2L,
-  #   str_detect(diagnostico,pattern = "\\+") ~ 1L,
-  #   str_detect(diagnostico,pattern = "Neg") ~ NA_integer_,
-  #   TRUE ~ 0L
-  # )) %>% 
-  # tail(25) %>% 
-  # select(-n) %>% 
   epihelper::print_inf()
+
+# propuestas
+hem %>% 
+  count(cruces_n,diagnostico) %>% 
+  epihelper::print_inf()
+
+hem %>% 
+  count(gametocytes_presence,diagnostico) %>% 
+  epihelper::print_inf()
+
 
 hem %>% 
   filter(!is.na(temperatura_celsius)) %>% 
@@ -61,12 +72,11 @@ hem %>%
 hem %>% 
   filter(!is.na(temperatura_celsius)) %>% 
   filter(group=="pviv") %>% 
-  select(group,num.visita,
-         temperatura_celsius:densidad_parasitaria_gametocitos,cruces_n) %>% 
+  select(-new.code,-fecha,-(hto.:diff_fecha)) %>% 
   compareGroups(formula = num.visita ~ .,data = .) %>% 
   createTable(show.all = T, 
-              show.n = F, 
-              show.p.trend = T, sd.type = 2,
+              show.n = F, show.p.overall = F,
+              show.p.trend = F, sd.type = 2,
               digits = 1)
 
 # __ falciparum -----------------------------------------------------------
@@ -74,12 +84,11 @@ hem %>%
 hem %>% 
   filter(!is.na(temperatura_celsius)) %>% 
   filter(group=="pfal") %>% 
-  select(group,num.visita,
-         temperatura_celsius:densidad_parasitaria_gametocitos,cruces_n) %>% 
+  select(-new.code,-fecha,-(hto.:diff_fecha)) %>% 
   compareGroups(formula = num.visita ~ .,data = .) %>% 
   createTable(show.all = T, 
-              show.n = F, 
-              show.p.trend = T, sd.type = 2,
+              show.n = F, show.p.overall = F,
+              show.p.trend = F, sd.type = 2,
               digits = 1)
 
 # figuras -----------------------------------------------------------------
@@ -108,7 +117,7 @@ hem %>%
                                dodge.width = 0.9)
 
 
-# trofosoitos -------------------------------------------------------------
+# trofozoitos -------------------------------------------------------------
 
 hem %>% 
   filter(group!="control") %>% 
@@ -130,6 +139,49 @@ hem %>%
                                dodge.width = 0.9)
 
 
+# gametocitos -------------------------------------------------------------
+
+hem %>% 
+  filter(group!="control") %>% 
+  ggplot(aes(x = num.visita,
+             y = densidad_parasitaria_gametocitos, 
+             group = new.code,
+             color = group)) +
+  geom_line() +
+  facet_wrap(~group)
+
+hem %>% 
+  filter(group!="control") %>% 
+  ggplot(aes(x = num.visita,
+             y = densidad_parasitaria_gametocitos, 
+             #group = new.code,
+             color = group)) +
+  geom_violin() +
+  ggbeeswarm::geom_quasirandom(method = "tukeyDense",
+                               dodge.width = 0.9)
+
+hem %>% 
+  filter(group!="control") %>% 
+  ggplot(aes(x = gametocytes_presence,
+             y = densidad_parasitaria_gametocitos, 
+             #group = new.code,
+             color = gametocytes_presence)) +
+  geom_violin() +
+  ggbeeswarm::geom_quasirandom(method = "tukeyDense",
+                               dodge.width = 0.9)
+
+hem %>% 
+  filter(group!="control") %>% 
+  ggplot(aes(x = gametocytes_presence,
+             y = densidad_parasitaria_gametocitos, 
+             #group = new.code,
+             color = gametocytes_presence)) +
+  geom_violin() +
+  ggbeeswarm::geom_quasirandom(method = "tukeyDense",
+                               dodge.width = 0.9) +
+  facet_wrap(~group)
+
+
 # cruces ------------------------------------------------------------------
 
 hem %>% 
@@ -145,6 +197,41 @@ hem %>%
   colorspace::scale_fill_discrete_sequential(palette = "Light Grays", 
                                              rev=FALSE) +
   # scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+hem %>% 
+  filter(group!="control") %>% 
+  mutate(cruces_n=fct_rev(cruces_n)) %>% 
+  ggplot(aes(x = num.visita,
+             # y = cruces_n, 
+             # group = new.code,
+             fill = cruces_n)) +
+  geom_bar(position = position_fill()) +
+  # geom_bar() +
+  facet_wrap(~group) +
+  colorspace::scale_fill_discrete_sequential(palette = "Light Grays", 
+                                             rev=FALSE) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+
+# gametocitos -------------------------------------------------------------
+
+hem %>% 
+  filter(group!="control") %>% 
+  # mutate(gametocytes_presence=fct_rev(gametocytes_presence)) %>%
+  ggplot(aes(x = num.visita,
+             # y = cruces_n, 
+             # group = new.code,
+             fill = gametocytes_presence)) +
+  geom_bar(position = position_fill()) +
+  # geom_bar() +
+  facet_wrap(~group) +
+  colorspace::scale_fill_discrete_sequential(palette = "Light Grays", 
+                                             rev=FALSE) +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank())
 
